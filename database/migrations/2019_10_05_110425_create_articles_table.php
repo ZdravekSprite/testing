16889cac58e7f984d37ed078bf4ef8f139806f5e
Zdravko Šplajt <zdravek.sprite@gmail.com>
2019-10-05 13:09:35 +0200

    edit migration

--- database/migrations/2019_10_05_110425_create_articles_table.php
+++ database/migrations/2019_10_05_110425_create_articles_table.php
@@ -6,26 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateArticlesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('articles', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('title');
+      $table->text('body');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('articles');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('articles');
+  }
 }
