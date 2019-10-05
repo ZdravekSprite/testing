11571c068992301b60bc7a8bc19c9340733c7f0a
Zdravko Šplajt <zdravek.sprite@gmail.com>
2019-10-05 13:20:14 +0200

    edit ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,82 +4,94 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use Illuminate\Http\Request;
+use App\Http\Resources\Article as ArticleResource;
 
 class ArticleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $articles = Article::paginate(15);
+	  return ArticleResource::collection($articles);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Article $article)
-    {
-        //
-    }
+    $article->id = $request->input('article_id');
+    $article->title = $request->input('title');
+    $article->body = $request->input('body');
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Article $article)
-    {
-        //
+    if($article->save()) {
+      return new ArticleResource($article);
     }
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Article $article)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Article $article)
+  {
+    return new ArticleResource($article);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Article $article)
-    {
-        //
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Article $article)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Article $article)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Article $article)
+  {
+    if($article->delete()) {
+      return new ArticleResource($article);
     }
+  }
 }
