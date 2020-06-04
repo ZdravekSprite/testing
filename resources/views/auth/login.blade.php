7ee49d246abb2ec33ff4f562084486af1cbb0e9e
Zdravko Splajt <zdravek.sprite@gmail.com>
2020-06-04 21:17:45 +0200

    login blade buttons [socialite-multi]

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 offset-md-3">
                                 <a href="login/facebook" class="btn btn-primary">
                                     {{ __('Login Fb') }}
                                 </a>
