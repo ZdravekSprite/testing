02f25ccf70a74d9c63aba65d67a13b02b18e8ae4
Zdravko Splajt <zdravek.sprite@gmail.com>
2020-06-04 21:34:18 +0200

    proviers_id user [socialite-multi]

--- app/User.php
+++ app/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'email', 'password',
+        'facebook_id', 'twitter_id', 'linkedin_id', 'google_id', 'github_id',
     ];
 
     /**
