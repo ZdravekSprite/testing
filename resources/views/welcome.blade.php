15064075bb35415b14cff15776fe24c0a2c7640e
Zdravko Splajt <zdravek.sprite@gmail.com>
2020-06-06 22:38:18 +0200

    welcome fonts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
