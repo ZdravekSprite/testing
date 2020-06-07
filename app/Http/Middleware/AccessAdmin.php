fe24000e01fe403ce7dbfd3a3d74e2cf573332f3
Zdravko Splajt <zdravek.sprite@gmail.com>
2020-06-08 01:51:32 +0200

    handle accessAdmin [access-admin]

--- app/Http/Middleware/AccessAdmin.php
+++ app/Http/Middleware/AccessAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Auth;
 
 class AccessAdmin
 {
@@ -15,6 +16,10 @@ class AccessAdmin
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(Auth::user()->hasAnyRole('superadmin', 'admin')){
+            return $next($request);
+        }
+      
+        return redirect('home');
     }
 }
