1f375e188972f73032bbe20076a36280e0bbaea2
Zdravko Splajt <zdravek.sprite@gmail.com>
2020-06-08 22:05:11 +0200

    start stop impersonate controller [access-admin]

--- app/Http/Controllers/Admin/ImpersonateController.php
+++ app/Http/Controllers/Admin/ImpersonateController.php
@@ -4,8 +4,33 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 class ImpersonateController extends Controller
 {
-    //
+    /**
+     * Start Impersonate the specified User.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function start($id)
+    {
+        $user = User::where('id', $id)->first();
+        if ($user) {
+            session()->put('impersonate', $user->id);
+        }
+        return redirect('/home');
+    }
+
+    /**
+     * Stop Impersonate.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function stop()
+    {
+        session()->forget('impersonate');
+        return redirect('/home');
+    }
 }
