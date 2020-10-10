8e42d66a251a9d2907ba5e4410cb39ea2a0ee2e1
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-10 18:41:19 +0200

    app [linkedin]

--- linkedInTestBack/index.js
+++ linkedInTestBack/index.js
@@ -1,11 +1,7 @@
+const app = require('./app') 
 const http = require('http')
 const config = require('./utils/config') 
 const logger = require('./utils/logger')
-
-const app = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
-}
 
 const server = http.createServer(app)
 
