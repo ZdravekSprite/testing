83cd019a7b3f9da78ed5066629c83f6182505c90
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-10-11 09:48:54 +0200

    let tests [linkedin]

---
+++ linkedInTestBack/README.md
@@ -0,0 +1,21 @@
+# LinkedIn Test Backend app
+
+```
+npm init --yes
+npm install --save-dev nodemon
+npm install dotenv
+```
+
+> .gitignore
+
+```
+node_modules
+.env
+```
+
+> package.json
+
+```
+"start": "node index.js",
+"dev": "nodemon index.js",
+```
--- linkedInTestBack/app.js
+++ linkedInTestBack/app.js
@@ -1,6 +1,51 @@
-const app = (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World')
+let tests = [
+  {
+    content: "OOP",
+    qa: [
+      {
+        q: ["What is an example of dynamic binding?", 0],
+        a1: ["method overriding", 0],
+        a2: ["any method", 0],
+        a3: ["method overloading", 0],
+        a4: ["compiling", 0]
+      },
+      {
+        q: ["For which case would the use of a static attribute be appropriate?", 0],
+        a1: ["the weather conditions for each house in a small neighborhood", 0],
+        a2: ["the number of people in each house in a small neighborhood", 0],
+        a3: ["the lot size for each house in a small neighborhood", 0],
+        a4: ["the color of each house in a small neighborhood", 0]
+      }
+    ]
+  },
+  {
+    content: "Java",
+    qa: [
+      {
+        q: ["Given the string \"strawberries\" saved in a variable called fruit, what would \"fruit.substring(2, 5)\" return?", 0],
+        a1: ["raw", 0],
+        a2: ["rawb", 0],
+        a3: ["awb", 0],
+        a4: ["traw", 0]
+      },
+      {
+        q: ["How can you achieve runtime polymorphism in Java?", 0],
+        a1: ["method overriding", 0],
+        a2: ["method overloading", 0],
+        a3: ["method overrunning", 0],
+        a4: ["method calling", 0]
+      }
+    ]
   }
-  
+]
+
+const app = (req, res) => {
+  /*
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello World')
+  */
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(tests))
+}
+
 module.exports = app
