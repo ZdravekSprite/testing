ed171c3fe40076f00d9a5cc87f1cccfe9878924c
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-11-15 20:32:58 +0100

    component Togglable [lotto]

--- lotto/lotto-frontend/src/components/Lottery.js
+++ lotto/lotto-frontend/src/components/Lottery.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Draw from './Draw'
+import Togglable from './Togglable'
 
 const Lottery = ({ lottery }) => {
+
+  const drawFormRef = React.createRef()
 
   return (
     <div className='lottery' >
       <h2>{lottery.name}</h2>
       <h3>{lottery.draws_info[0]}/{lottery.draws_info[1]}:{lottery.bonus_info[0]}/{lottery.bonus_info[1]}</h3>
+      <Togglable buttonLabel='add new draw' ref={drawFormRef}>
+        <p>Add draw</p>
+      </Togglable>
       <ul>
         {lottery.draws.map((draw, i) =>
           <Draw
