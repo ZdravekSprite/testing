347c8cd44496a95d33cc93c23ffd0585b2e84075
Zdravko Šplajt <zdravek.sprite@gmail.com>
2020-11-15 19:38:48 +0100

    axios [lotto]

--- lotto/lotto-frontend/src/App.js
+++ lotto/lotto-frontend/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import lotteryService from './services/lotteries'
+
 const App = () => {
+  const [lotteries, setLotteries] = useState([])
+
+  useEffect(() => {
+    lotteryService
+      .getAll()
+      .then(initialLotteries => {
+        setLotteries(initialLotteries)
+      })
+  }, [])
+
+  console.log(lotteries)
 
   return (
     <div>
       <h1>Lotto</h1>
+      <h2>{lotteries.name}</h2>
+      <h3>{lotteries.draws_info[0]}/{lotteries.draws_info[1]}:{lotteries.bonus_info[0]}/{lotteries.bonus_info[1]}</h3>
+      <ul>
+        {lotteries.draws.map((lottery, i) =>
+          <li
+            key={i}
+            className='lottery'
+          >
+          {lottery.date} 
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
